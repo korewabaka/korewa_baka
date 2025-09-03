b08b91689bb7383385826b7b7813334ad1dee00e
korewabaka <pihubaka712@gmail.com>
2025-09-03 16:27:34 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+a=float(input('Enter Number 1: '))
+b=float(input('Enter Number 2: '))
+Addition= a+b
+print("Addition: ", a+b)
+Subtraction= a-b
+print("Subtraction: ", a-b)
+Multiplication= a*b
+print("Multiplication: ", a*b)
+Division= a/b
+print("Division: ", a/b)
